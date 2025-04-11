52f5340d28c13bf044c8d6742608621d44ae69ed
1N4 <sample@bths.edu>
2025-04-11 10:45:01 -0400

    commit

---
+++ Apple Project/UpgradesView.swift
@@ -0,0 +1,18 @@
+//
+//  UpgradesView.swift
+//  Apple Project
+//
+//  Created by Student on 4/11/25.
+//
+
+import SwiftUI
+
+struct UpgradesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpgradesView()
+}
