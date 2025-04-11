76a370bcd0568c5bc7f35105d4a5c2f315a84490
1N4 <sample@bths.edu>
2025-04-11 08:48:50 -0400

    Initial Commit

---
+++ Apple Project/Apple_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_ProjectApp.swift
+//  Apple Project
+//
+//  Created by Student on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
