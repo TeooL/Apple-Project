5dc930143f79a30048f4471a1fcb9c0f0f8c9c6f
1N4 <sample@bths.edu>
2025-04-11 10:17:52 -0400

    added player stuff

--- Apple Project/Player.swift
+++ Apple Project/Player.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class Player: ObservableObject {
+    @Published var damage: Int = 10
+    @Published var attackSpeed: Int = 10
+    @Published var bulletCount: Int = 1
+    
+    func damageUpgrade() {
+        damage += 2
+    }
+    
+    func attackSpeedUpgrade() {
+        attackSpeed += 2
+    }
+    func bulletCountUpgrade() {
+        bulletCount += 1
+    }
+}
