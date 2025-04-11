b5eb885444bbee8ea91859325d66eeaa8590eaac
1N4 <sample@bths.edu>
2025-04-11 10:10:06 -0400

    GameView, MainMenu, and Shape Class

---
+++ Apple Project/MainMenu.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenu.swift
+//  Apple Project
+//
+//  Created by Student on 4/11/25.
+//
+
+import SwiftUI
+
+struct MainMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenu()
+}
---
+++ Apple Project/Shape.swift
@@ -0,0 +1,8 @@
+//
+//  Shape.swift
+//  Apple Project
+//
+//  Created by Student on 4/11/25.
+//
+
+import Foundation
