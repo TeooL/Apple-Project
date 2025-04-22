ca20347004813a0f7643c82b5bb78329075f5127
Jonathan <redcarpet@bths.edu>
2025-04-22 17:31:37 -0400

    updated1

---
+++ Apple Project/EditDetails.swift
@@ -0,0 +1,18 @@
+//
+//  EditDetails.swift
+//  Apple Project
+//
+//  Created by Student on 4/22/25.
+//
+
+import SwiftUI
+
+struct EditDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDetails()
+}
