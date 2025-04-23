ac91ea2287018c0f9f24fbb7ebf65f911b1b15fb
Jonathan <redcarpet@bths.edu>
2025-04-23 18:06:30 -0400

    finished code, need design

---
+++ Apple Project/DailyChallengesView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyChallengesView.swift
+//  Apple Project
+//
+//  Created by Student on 4/23/25.
+//
+
+import SwiftUI
+
+struct DailyChallengesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyChallengesView()
+}
