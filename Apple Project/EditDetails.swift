d0ea5695e7779952abd7c1d8df07e791ce0a2441
Jonathan <redcarpet@bths.edu>
2025-04-24 08:46:36 -0400

    design

--- Apple Project/EditDetails.swift
+++ Apple Project/EditDetails.swift
@@ -20,11 +20,29 @@ struct EditDetails: View {
             VStack {
                 TextField("Title", text: $newTitle)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .overlay(
+                        RoundedRectangle(cornerRadius:5)
+                            .stroke(Color.brown, lineWidth: 2)
+                    )
+                    .shadow(radius: 5)
+                    .padding()
                 TextField("Category", text: $newCategory)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .overlay(
+                        RoundedRectangle(cornerRadius:5)
+                            .stroke(Color.brown, lineWidth: 2)
+                    )
+                    .shadow(radius: 5)
+                    .padding()
                 TextField("Details", text: $newDetails, axis: .vertical)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .lineLimit(10...15)
+                    .overlay(
+                        RoundedRectangle(cornerRadius:5)
+                            .stroke(Color.brown, lineWidth: 2)
+                    )
+                    .shadow(radius: 5)
+                    .lineLimit(15...20)
+                    .padding()
                 Spacer()
                 Button(action: {
                     viewModel.updateHabit(habit: habit, details: newDetails, title: newTitle, category: newCategory)
@@ -33,12 +51,18 @@ struct EditDetails: View {
                         .font(.headline)
                         .frame(maxWidth: .infinity)
                         .padding()
-                        .foregroundColor(.white)
-                        .background(Color.green)
+                        .foregroundColor(.yellow)
+                        .background(
+                            LinearGradient(colors: [.orange, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
                         .cornerRadius(10)
                 }
             }
             .padding()
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color.brown.opacity(0.7), Color.orange.opacity(0.5)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .edgesIgnoringSafeArea(.all)
+            )
         }
     }
 }
